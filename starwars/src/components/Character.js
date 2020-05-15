dddbcabd4cac7594bc1bc6451c0886c8012481c5
Barbara Moore <barbaraloismoore@gmail.com>
2020-05-15 13:20:50 -0400

    completed MVP and pagination

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react';
+import { CharacterBlock } from '../styled-components';
+
+export default function Character(props) {
+  return (
+    <CharacterBlock>
+      <img
+        src={
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
+          props.id +
+          '.png'
+        }
+        alt={'A sprite of the Pokemon' + props.name}
+      />
+      <h2>{props.name}</h2>
+    </CharacterBlock>
+  );
+}
---
+++ starwars/src/styled-components.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const CharacterBlock = styled.div`
+  background-color: white;
+  width: 100px;
+  margin: 1rem;
+  border: 1px solid navy;
+
+  h2 {
+    font-size: 1rem;
+    padding: 0.5rem;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: navy;
+  color: white;
+  font-size: 1.2rem;
+  width: 100px;
+  padding: 0.2rem;
+  border: 1px solid white;
+  margin: 0.2rem;
+`;
